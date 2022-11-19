897016821397df9d5f6b78caff3aa4e806c32bf2
Aras Valizadeh <116960127+ArasValizadeh@users.noreply.github.com>
2022-11-19 21:06:54 +0330

    Update prime number.py

--- prime number.py
+++ prime number.py
@@ -1,12 +1,14 @@
 def CheckAval (number):
-    counter = 0
-    ma = int((number/2)+2)
-    for i in range (1,ma):
-        baghi = number % i
-        if baghi == 0 :
-            counter += 1
-    if counter == 1 :
-        return number 
+    if number != 1 :
+        counter = 0
+        ma = number
+        for i in range (1,ma):
+            baghi = number % i
+            if baghi == 0 :
+                counter += 1
+        if counter == 1 :
+            return number
+ 
 a = int(input())
 b = int(input())
 ListAdad = []
